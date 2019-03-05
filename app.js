aa405b2020cbe686e10584353865f71dd796c725
Chance Harmon <henrikillsit@gmail.com>
2019-03-04 20:44:41 -0800

    pretty close math I think

--- app.js
+++ app.js
@@ -1,53 +1,74 @@
 'use strict';
+console.log('script loaded');
 
-var busMall = document.getElementById('bus-mall');
+var busMall1 = document.getElementById('busmall1');
+var busMall2 = document.getElementById('busmall2');
+var busMall3 = document.getElementById('busmall3');
 
 var allPics = [];
 
 function MallPics(name, type) {
-this.filepath = `images/${name}${type}`;
-this.name = name;
-this.views = 0;
-allPics.push(this);
+  this.filepath = `images/${name}${type}`;
+  this.name = name;
+  this.type = type;
+  this.views = 0;
+  allPics.push(this);
+}
 
-
-new MallPic('bag', 'bag.jpg');
-new MallPic('banana', 'banana.jpg');
-new MallPic('bathroom', 'bathroom.jpg');
-new MallPic('boots', 'boots.jpg');
-new MallPic('breakfast', 'breakfast.jpg');
-new MallPic('bubblegum', 'bubblegum.jpg');
-new MallPic('chair', 'chair.jpg');
-new MallPic('cthulhu', 'cthulhu.jpg');
-new MallPic('dog-duck', 'dog-duck.jpg');
-new MallPic('dragon', 'dragon.jpg');
-new MallPic('pen', 'pen.jpg');
-new MallPic('pet-sweep', 'pet-sweep.jpg');
-new MallPic('scissors', 'scissors.jpg');
-new MallPic('shark', 'shark.jpg');
-new MallPic('sweep', 'sweep.png');
-new MallPic('tauntaun', 'tauntaun.jpg');
-new MallPic('unicorn', 'unicorn.jpg');
-new MallPic('usb', 'usb.gif');
-new MallPic('water-can', 'water-can.jpg');
-new MallPic('wine-glass', 'wine-glass.jpg');
+new MallPics('bag', '.jpg');
+new MallPics('banana', '.jpg');
+new MallPics('bathroom', '.jpg');
+new MallPics('boots', '.jpg');
+new MallPics('breakfast', '.jpg');
+new MallPics('bubblegum', '.jpg');
+new MallPics('chair', '.jpg');
+new MallPics('cthulhu', '.jpg');
+new MallPics('dog-duck', '.jpg');
+new MallPics('dragon', '.jpg');
+new MallPics('pen', '.jpg');
+new MallPics('pet-sweep', '.jpg');
+new MallPics('scissors', '.jpg');
+new MallPics('shark', '.jpg');
+new MallPics('sweep', '.png');
+new MallPics('tauntaun', '.jpg');
+new MallPics('unicorn', '.jpg');
+new MallPics('usb', '.gif');
+new MallPics('water-can', '.jpg');
+new MallPics('wine-glass', '.jpg');
 
 function showRandomPic() {
-var random = Math.floor(Math.random() * allPics.length);
-allPics.src = allPics[random].filepath;
-allPics.alt = allPics[random].name;
-busMall.title = allPics[random].name;
-allPics[random].views++;
-console.log('current goat, ', allPics[random]);
+var random1 = Math.floor(Math.random() * allPics.length);
+busMall1.src = allPics[random1].filepath;
+busMall1.alt = allPics[random1].name;
+busMall1.title = allPics[random1].name;
+allPics[random1].views++;
 
+var random2 = Math.floor(Math.random() * allPics.length);
+while (random2 === random1) {
+  random2 = Math.floor(Math.random()*allPics.length);
 }
+busMall2.src = allPics[random2].filepath;
+busMall2.alt = allPics[random2].name;
+busMall2.title = allPics[random2].name;
+allPics[random2].views++;
+
+var random3 = Math.floor(Math.random() * allPics.length);
+while (random2 === random1 && random3 === random2) {
+  random2 = Math.floor(Math.random()*allPics.length);
+}
+busMall3.src = allPics[random3].filepath;
+busMall3.alt = allPics[random3].name;
+busMall3.title = allPics[random3].name;
+allPics[random3].views++;
+};
 
 showRandomPic();
 
-busMall.addEventListener('click', handleClick);
+busMall1.addEventListener('click', handleClick);
+busMall2.addEventListener('click', handleClick);
+busMall3.addEventListener('click', handleClick);
 
 function handleClick(event) {
   console.log('target, ', event.target);
   showRandomPic();
-}
 }
